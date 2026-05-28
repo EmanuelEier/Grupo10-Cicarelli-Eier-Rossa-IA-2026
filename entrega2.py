8a7d6423af236a39a9b25045b736ec43a8309304
Tomi <tomasdiegorossa@gmail.com>
2026-05-28 11:16:35 -0300

    Agregamos algunos comentarios y armamos las conclusiones de la entrega 2

--- entrega2.py
+++ entrega2.py
@@ -5,7 +5,7 @@ from simpleai.search import CspProblem, backtrack, MOST_CONSTRAINED_VARIABLE, LE
 
 def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
     variables = []
-
+    # Variables: una por cada módulo a ubicar
     for i in range(habs):
         variables.append(f"hab_{i}")
 
@@ -26,6 +26,7 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
     def borde_mapa(fila, columna):
         return fila == camp_size[0]-1 or fila == 0 or columna == camp_size[1]-1 or columna == 0
 
+    # Dominios: celdas válidas para cada variable (sin cráteres, respetando borde/interior)
     for variable in variables:
         dominios[variable] = []
         for fila in range(camp_size[0]):
@@ -39,9 +40,10 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
                             dominios[variable].append((fila, columna))
                     else:
                         dominios[variable].append((fila, columna))
-
+    # Restricciones
     restricciones = []
 
+    # Sin superposición
     def no_superposicion(variables, valores):
         return valores[0] != valores[1]
 
@@ -57,9 +59,10 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
 
         return not (abs(f1 - f2) + abs(c1 - c2) == 1)
 
+    # Generador no adyacente a habitacional
     for v1, v2 in itertools.product(tuple(variables_gen), tuple(variables_hab)):
         restricciones.append(((v1, v2), no_adyacentes))
-
+    #, ni generadores entre sí
     for v1, v2 in itertools.combinations(variables_gen, 2):
         restricciones.append(((v1, v2), no_adyacentes))
 
@@ -77,6 +80,7 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
         ]
         return any(adyacente in posiciones_dep for adyacente in adyacentes)
 
+    # Cada laboratorio debe tener al menos un depósito adyacente
     if variables_lab and variables_dep:
         for lab in variables_lab:
             restricciones.append((tuple([lab] + variables_dep), tiene_depositos_adyacentes))
@@ -99,6 +103,7 @@ def build_camp(camp_size, habs, generators, labs, deposits, airlocks, craters):
             for nf, nc in adyacentes
         )
 
+    # Cada habitacional debe tener al menos una celda libre adyacente
     for hab in variables_hab:
         otras = [v for v in variables if v != hab]
         restricciones.append((tuple([hab] + otras), tiene_algun_adyacente_libre))
